3ec4bdb484df1acd4804dd257ba62a2d444eb6c9
stelmistery <stelmistery@yandex.ru>
2020-05-12 23:41:06 +0300

    Added panel template

--- gorizont/settings.py
+++ gorizont/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'main',
     'book',
     'formtools',
+    'panel',
 ]
 
 MIDDLEWARE = [
--- gorizont/urls.py
+++ gorizont/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('book/', include('book.urls')),
     path('', index),
+    path('panel/', include('panel.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
